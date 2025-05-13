6017435db080fbd2b1f91e6224cade2874e69e23
Jean-Marie Roy <jeanmroy@gmail.com>
2025-05-13 15:20:05 -0400

    Initial commit - scaffold using vite.js

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Élan Mauricie</h1>
+      <div>
+        <h3>
+          Bientôt en ligne!
+        </h3>
+      </div>
+
+    </>
+  )
+}
+
+export default App
